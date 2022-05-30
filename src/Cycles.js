66f06f1a8ac24a3e1950e8df587a77735d75bced
oa.filimonova <filimonovao127@gmail.com>
2022-05-30 19:15:42 +0300

    Task Cycles

--- src/Cycles.js
+++ src/Cycles.js
@@ -4,26 +4,26 @@
   и верните итоговый результат.
 */
 export function rangeSum(start, end) {
-  let sum = 0;
-  start % 2 == 0 ? null : (start += 1);
-  for (let i = start; i <= end; i += 2) {
-      sum += i;
-  }
-  return sum;
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        if (i % 2 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
 }
-
 /*
 В функцию iterationCount() приходит неотрицательное число.
 Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
 и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
 */
 export function iterationCount(a) {
-  let result = 0;
-  while (a > 0.1) {
-      a = a / 2;
-      result++;
-  }
-  return result;
+    let result = 0;
+    while (a > 0.1) {
+        a = a / 2;
+        result++;
+    }
+    return result;
 }
 /*
 В функцию symbolsReplace() приходит строка текста.
@@ -31,15 +31,15 @@ export function iterationCount(a) {
 и верните итоговый результат.
 */
 export function symbolsReplace(message) {
-  let str = '';
-  let a = 0;
-  do {
-      if ((a + 1) % 3 == 0) {
-          str += '_';
-      } else {
-          str += message[a];
-      }
-      a++;
-  } while (a < message.length);
-  return str;
+    let str = '';
+    let a = 0;
+    do {
+        if ((a + 1) % 3 == 0) {
+            str += '_';
+        } else {
+            str += message[a];
+        }
+        a++;
+    } while (a < message.length);
+    return str;
 }
